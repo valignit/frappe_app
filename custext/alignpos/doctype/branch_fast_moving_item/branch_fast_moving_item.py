c1dbfef4a87fe5f5c1a4c9ac8a575e1bc95d65ab
valignit <ghouseam@gmail.com>
2022-07-28 11:43:24 +0000

    reset

---
+++ custext/alignpos/doctype/branch_fast_moving_item/branch_fast_moving_item.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, VAlignIt and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class BranchFastMovingItem(Document):
+	pass
