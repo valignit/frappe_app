57e5f182df2658dde4e5efdf664b99c9fd9d2a17
VAlignIt Technologies <ghouseam@gmail.com>
2022-08-09 03:58:34 +0300

    Delete custext/alignpos directory

--- custext/alignpos/doctype/alignpos_invoice/alignpos_invoice.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, VAlignIt and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Alignpos Invoice', {
-	// refresh: function(frm) {
-
-	// }
-});
--- custext/alignpos/doctype/branch_fast_moving_item/branch_fast_moving_item.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, VAlignIt and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class BranchFastMovingItem(Document):
-	pass
